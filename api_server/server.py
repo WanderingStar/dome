f3381040824cb76cdbfdbd323dafe92405fece2c
Aneel Nazareth <bitbucket@account.loath.org>
2015-05-09 18:18:10 -0500

    include ids in play

--- api_server/server.py
+++ api_server/server.py
@@ -99,7 +99,13 @@ def play():
             db.playing.replace_one({}, {'keywords': []}, upsert=True)
         else:
             db.playing.replace_one({}, {'keywords': sorted(list(keywords))}, upsert=True)
-    return jsonify(db.playing.find_one({}, {'_id': 0}))
+    playlist = db.playing.find_one({}, {'_id': 0})
+    keywords = playlist.get('keywords')
+    query = {}
+    if keywords:
+        query = {'keywords': {'$all': keywords}}
+    ids = sorted([d['id'] for d in db.post.find(query, {'id':1, '_id':0})])
+    return jsonify({'keywords': keywords, 'ids': ids})
 
 @app.after_request
 def add_cors(resp):
